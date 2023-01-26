9262328d98d4c09ea8d6ded689439c1710164455
Lucas de Pádua <72556348+Padualb@users.noreply.github.com>
2023-01-26 08:47:36 -0300

    Angular project

---
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -0,0 +1,35 @@
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+
+export class ProductCreateComponent implements OnInit{
+  
+  product: Product = {
+    name: '',
+    price: null
+
+  }
+  
+  constructor(private productService: ProductService, private router: Router){}
+  
+  ngOnInit(): void {
+    
+    
+  }
+  
+  createProduct(): void{
+
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado')
+      this.router.navigate(['/products'])
+    })
+    
+  }
+}
